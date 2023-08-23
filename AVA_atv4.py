2896cfa56275c395c53a4ac0f8ac7dd21aa09a56
Micaela Milman <142030626+MicaMilman@users.noreply.github.com>
2023-08-23 16:12:14 -0300

    Update AVA_atv4.py

--- AVA_atv4.py
+++ AVA_atv4.py
@@ -1,10 +1,11 @@
-print('\n Controle de Colaboradores da Micaela Milman')
+print('Controle de Colaboradores da Micaela Milman')
 
 lista_colaboradores = []
 id_colaborador = 0
 
 
 def cadastrar_colaboradores(id):  #função de cadastro do colaborador
+    print('\n---------- MENU DE CADASTRO ----------\n')
     print('Cadastre o Colaborador: ')
     nome = input('Qual o nome do colaborador: ')
     setor = input('Qual o setor de trabalho do colaborador: ')
@@ -18,23 +19,22 @@ def cadastrar_colaboradores(id):  #função de cadastro do colaborador
     }
     lista_colaboradores.append(dicionario_colaborador.copy())
 def consultar_colaboradores():  #consulta de colaborador
+    print('\n---------- MENU DE CONSULTA ----------\n')
     print('Consultar Colaborador: ')
     while True:  # loop de menu para consulta colaborador(a)
-        consult_colab = input('''Digite a opcão desejada: \n 
-                                       1 - Consultar todos os colaboradores\n
-                                       2 - Consultar por Id\n
-                                       3 - Consultar por setor\n
-                                       4 - Retornar ao menu principal\n
+        consult_colab = input('''Digite a opcão desejada:
+                                       1 - Consultar todos os colaboradores
+                                       2 - Consultar por Id
+                                       3 - Consultar por setor
+                                       4 - Retornar ao menu principal
                                        >>> ''') #menu
         if consult_colab == '1':  #se consultar todos
-            print('Você escolheu consultar todos os colaboradores: ')
             for colaborador in lista_colaboradores:
                 print('-' * 30)
                 for key, value in colaborador.items():
                     print(f'{key} : {value}')
                 print('-' * 30)
         elif consult_colab == '2':  #se consultar por id
-            print('Você escolheu consultar por Id: ')
             id = int(input('Digite o ID do colaborador: '))
             for colaborador in lista_colaboradores:
                 if colaborador['id'] == id:
@@ -43,7 +43,6 @@ def consultar_colaboradores():  #consulta de colaborador
                         print(f'{key} : {value}')
                     print('-' * 30)
         elif consult_colab == '3':  #se consultar por setor
-            print('Você escolheu consultar por Setor: ')
             setor_colab = input('Digite o Setor do colaborador: ')
             for setor in lista_colaboradores:
                 if setor['setor'] == setor_colab:
@@ -51,28 +50,28 @@ def consultar_colaboradores():  #consulta de colaborador
                     for key, value in setor.items():
                         print(f'{key} : {value}')
                     print('-' * 30)
-        elif consult_colab == '4':  #voltar pro menú
+        elif consult_colab == '4':  #voltar pro menu
             return
         else:  #erro que volta pro inicio do laço
             print('Opção inválida. Tente novamente')
             continue
 def remover_colaboradores():  #função remover colaborador
-    print('Você escolheu remover colaborador: ')
-    id = int(input('Entre com o ID do colaborador que deseja remover: '))
+    id = int(input('Digite o ID do colaborador que será removido: '))
     for colaborador in lista_colaboradores:
         if colaborador['id'] == id:
             lista_colaboradores.remove(colaborador)
-            print('Colaborador REMOVIDO com sucesso!')
+            print('\nColaborador REMOVIDO com sucesso!\n')
 
 #programa principal:
 
 while True:  #menu principal
+    print('\n---------- MENU PRINCIPAL ----------\n')
     print('Cadastro, consulta ou remoção de colaborador')
-    menu = input('''Digite a opcão desejada: \n
-                 1 - Cadastrar colaborador\n
-                 2 - Consultar Colaborador\n
-                 3 - Remover colaborador\n
-                 4 - Encerrar programa\n
+    menu = input('''Digite a opcão desejada:
+                 1 - Cadastrar colaborador
+                 2 - Consultar Colaborador
+                 3 - Remover colaborador
+                 4 - Encerrar programa
                  >>> ''') #menu
 
     if menu == '1':  # Cadastro
